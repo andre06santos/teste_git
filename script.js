d7ed51fd11877d97e9c6c04672e5b65421eb9b43
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-10-16 12:54:58 -0300

    :recycle: refactor: refatoracao de funcoes e mensagens de erro :sparkles: feat: add toast  de erro

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
-// const cpfInput = document.getElementById('cpf')
-// const nomeInput = document.getElementById('nome')
-// const alturaInput = document.getElementById('altura')
-// const idadeInput = document.getElementById('idade')
-let errorMessages = []
-
-
-
 const processarDados = (e) => {
     e.preventDefault()
-
+    
     const cpfInput = document.getElementById('cpf')
     const nomeInput = document.getElementById('nome')
-    const idadeInput = document.getElementById('idade') 
     const alturaInput = document.getElementById('altura')
-    const colorSelect = document.querySelector('#corPele')
+    const idadeInput = document.getElementById('idade')
+    const corInput = document.querySelector('#corPele')
     
-    
-    const inputs = [cpfInput, nomeInput, idadeInput, alturaInput, colorSelect]
-    const inputsNames = ['Cpf', 'Nome', 'Idade', 'Altura', 'Cor de pele']
-    
-    createErrorMessages(inputs, validationFunctions(), inputsNames)
+    const inputs = [cpfInput, nomeInput, idadeInput, alturaInput, corInput]
+    const inputsNames = ['Cpf', 'Nome', 'Idade', 'Altura', 'Cor da pele']
+    const validationFunctions = [validateCpf, validateNome, validateIdade, validateAltura, validateCor]
 
-    isValidForm() ? createDataList(inputs, inputsNames) : showErrorMessages()
+    let errorMessages = createErrorMessages(inputs, inputsNames, validationFunctions)
+    
+    isValidForm(errorMessages) ? createDataList(inputs, inputsNames) : showErrorMessages(errorMessages)
     cleanInputs()
-    errorMessages = []
 }
 
 addEventListener("submit", processarDados)
 
 const acceptOnlyNumbers = (e) => {
+    containsOnlyLetters(e.target.value) && showToast("Digite apenas numeros!")
     e.target.value = e.target.value.replace(/\D/g, '')
 }
 
 const acceptOnlyText = (e) => {
+    containsOnlyNumbers(e.target.value) && showToast("Digite apenas letras!")
     e.target.value = e.target.value.replace(/[^a-zA-ZÀ-ÖØ-öø-ÿ\s]/g, '')
 }
 
-
 document.getElementById('cpf').addEventListener('input', acceptOnlyNumbers)
 document.getElementById('nome').addEventListener('input', acceptOnlyText)
-document.getElementById('idade') .addEventListener('input', acceptOnlyNumbers)
 document.getElementById('altura').addEventListener('input', acceptOnlyNumbers)
+document.getElementById('idade') .addEventListener('input', acceptOnlyNumbers)
+
+
+const showToast = (message) => {
+    const toast = document.createElement('p')
+    toast.innerText = message
+    toast.className = 'toast'
+
+    const time = 3000
+
+    const hasToast = document.querySelector('.toast') !== null
+
+    if (! hasToast){
+        document.body.appendChild(toast)
+    }
+
+    setTimeout(() => {
+        toast.remove()
+    }, time)
+
+}
 
 
 const validateCpf = (cpfValue) => {
@@ -71,133 +83,80 @@ const validateIdade = (idadeValue) => {
     return isValidIdade
 }
 
-const validateCor = () => {
-    const colorSelect = document.querySelector('#corPele')
-    const listaCores = []
-    for (let i = 0; i < colorSelect.options.length; i++){
-        listaCores.push(colorSelect.options[i].value)
-    }
-    const corSelecionada = colorSelect.value
-    const corValida = listaCores.includes(corSelecionada)
-
-    if (corValida){
-        return true
-    } else{
-        createErrorMessages(corSelecionada)
-    }
-    return corValida
+const validateCor = (corValue) => {
+    const validColors = ['BR', 'PD', 'NG']
+    const isValidCor = validColors.includes(corValue)
+    return isValidCor
 }
 
-const createErrorMessages = (inputs, validationFunctions, inputsNames) => {
 
+const createErrorMessages = (inputs, inputsNames, validationFunctions) => {
+    let errorMessages = []
+    
     for (let i = 0; i < inputs.length; i++){
         const actualValidationFunction = validationFunctions[i]
         const actualInput = inputs[i]
 
-        if (actualInput !== 'Cor de pele')
-
-            if (!actualValidationFunction(actualInput.value)){
-                errorMessages.push(`Campo de ${inputsNames[i]} com dados inválidos!`)
-            }
-        else{
-            validateCor()
+        if (!actualValidationFunction(actualInput.value)){
+            errorMessages.push(`Campo de ${inputsNames[i]} com dados inválidos!`)
         }
     }
-
-
-    // if (!validateCpf(cpfInput.value)){
-    //     errorMessages.push('CPF inválido')
-    // } 
-    // if (!validateNome(nomeInput.value)){
-    //     errorMessages.push('Nome inválido')
-    // } 
-    // if (!validateAltura(alturaInput.value)){
-    //     errorMessages.push('Altura inválida')
-    // } 
-    // if (!validateIdade(idadeInput.value)){
-    //     errorMessages.push('Idade inválida')
-    // } 
-    // if (!validateCor()){
-    //     errorMessages.push('Cor inválida')
-    // }
     
+    return errorMessages
 }
 
-const showErrorMessages = () => {
-    if (isValidForm()){
-        return
-    }
-    // createErrorMessages()
-    let text = ''
-    for (let error of errorMessages){
-        text += error + '\n'
-    }
-    alert(text)
+const showErrorMessages = (errorMessages) => {
+    alert(errorMessages[0])
 }
 
 const createDataList = (inputs, inputsNames) => {
     const container = document.getElementById("showForm")
     const list = document.createElement('ul')
     
-    if (container != null){
-        container.appendChild(list)
-        
+    if (container){
         const elementos = []
-
+        
         for (let i = 0; i < inputs.length; i++){
             const actualInput = inputs[i]
             const actualInputName = inputsNames[i]
-            if (actualInputName !== 'Cor de pele'){
+            const SKIN_COLOR = "Cor da pele"
+            if (actualInputName !== SKIN_COLOR){
                 elementos.push(`${actualInputName}: ` + actualInput.value)
             }
             else{
                 const selectInput = document.getElementById('corPele')
-                elementos.push("Cor da pele: " + selectInput.options[selectInput.selectedIndex].text)
+                const corSelecionada = selectInput.options[selectInput.selectedIndex].text
+                elementos.push(SKIN_COLOR + ": " + corSelecionada)
             }
-        }
+        } 
         
-        // validateCpf(cpfInput.value) ? elementos.push("CPF: " + cpfInput.value) : null
-        // validateNome(nomeInput.value) ? elementos.push("Nome: " + nomeInput.value) : null
-        // validateIdade(idadeInput.value) ? elementos.push("Idade: " + idadeInput.value) : null
-        // validateAltura(alturaInput.value) ? elementos.push("Altura: " + alturaInput.value) : null
-        // const selectInput = document.getElementById('corPele')
-        // validateCor() ? elementos.push("Cor da pele: " + selectInput.options[selectInput.selectedIndex].text) : null
-    
-
         for (let elemento of elementos){
             let li = document.createElement('li')
             li.innerText = elemento
             list.appendChild(li)
         }
         
-
+        container.appendChild(list)
     }
 
+    document.body.appendChild(container)
 
 }
 
-const isValidForm = () => {
+const isValidForm = (errorMessages) => {
     return errorMessages.length == 0
 }
 
 const cleanInputs = () => {
-    cpf.value = ''
-    nome.value = ''
-    idade.value = ''
-    altura.value = ''
+    const form = document.getElementById('myForm')
+    form.reset()
 }
-
 
 
 const containsOnlyNumbers = (str) => {
-    return /^\d+$/.test(str)
+    return /[^a-zA-ZÀ-ÖØ-öø-ÿ\s]/g.test(str)
 }
 
 const containsOnlyLetters = (str) => {
-    return /^\D+$/.test(str)
-}
-
-
-const validationFunctions = () => {
-    return [validateCpf, validateNome, validateIdade, validateAltura, validateCor]
+    return /\D/g.test(str)
 }
